24c8b9c27f3921512be47368c85385acce958fad
ViktorZvonar <viktorzvonar@yahoo.com>
2023-12-03 17:25:11 +0200

    Update saveLibrary.test.js

--- testing/tests/saveLibrary.test.js
+++ testing/tests/saveLibrary.test.js
@@ -14,3 +14,24 @@ test("saveLibrary should save the library to localStorage", () => {
 		JSON.stringify(myLibrary)
 	);
 });
+
+test("saveLibrary should handle an empty library", () => {
+	global.myLibrary = [];
+
+	saveLibrary();
+
+	expect(global.localStorage.setItem).toHaveBeenCalledWith(
+		"myLibrary",
+		JSON.stringify([])
+	);
+});
+
+test("saveLibrary should handle errors from localStorage", () => {
+	global.localStorage.setItem.mockImplementation(() => {
+		throw new Error("localStorage is not available");
+	});
+
+	expect(() => {
+		saveLibrary();
+	}).toThrow("localStorage is not available");
+});
